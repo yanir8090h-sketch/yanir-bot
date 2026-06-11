870df88744e5869133f86480bd787dd85f6fc1df
yanir8090h-sketch <yanir8090h@gmail.com>
2026-06-11 15:17:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -179,8 +179,8 @@ async def staff_request(ctx):
     await ctx.send("✅ בקשת הסטאף שלך נשלחה בהצלחה לחדר האישורים של הלנה!")
 
 # שימוש במשתנה המאובטח מה-Variables
-import os
-bot.run(os.environ.get("DISCORD_TOKEN")) MTQ4MDMzMjIxMTQyODUyODI0OA.G4K6e9.Y-dRiBvKE7gkIj5L9Cwt-hrxNv-O5pORj0zmnY
+bot.run(os.environ.get("MTQ4MDMzMjIxMTQyODUyODI0OA.G4K6e9.Y-dRiBvKE7gkIj5L9Cwt-hrxNv-O5pORj0zmnY")
+
 
 
 
