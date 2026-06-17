7bd32c0538f11f83cffbad8bc7956945eb3aee55
yanir8090h-sketch <yanir8090h@gmail.com>
2026-06-17 12:26:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class HelpButtonView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
+    
     @discord.ui.button(label="\u05d8\u05e4\u05dc \u05db\u05d0\u05df \u2694\ufe0f", style=discord.ButtonStyle.success, custom_id="take_help_call")
     async def take_call(self, interaction: discord.Interaction, button: discord.ui.Button):
         embed = interaction.message.embeds[0]
